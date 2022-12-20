c07a1a5617e3253456e826c67c7900b7bdb2a10c
Merdan D <merdan.durdiyev@gmail.com>
2022-12-20 20:32:19 +0500

    Adds Faker gem and prepares 'seeds.rb' file with fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+User.create(email: "merdan.durdiyev@gmail.com", password: "halowelt", password_confirmation: "halowelt")
+User.create(email: "mdzine@gmail.com", password: "halamonda", password_confirmation: "halamonda")
+User.create(email: "mdyc@yahoo.com", password: "salamdunya", password_confirmation: "salamdunya")
+
+10.times do
+  User.create(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    password_confirmation: Faker::Internet.password
+  )
+end
+
+
+User.all.each do |user| 
+  10.times do
+    user.recipes.create!(
+      title: Faker::Food.dish,
+      brief_info: Faker::Lorem.sentence,
+      description: Faker::Food.description,
+      is_halal: Faker::Boolean.boolean,
+      is_kosher: Faker::Boolean.boolean,
+      is_vegetarian: Faker::Boolean.boolean,
+    )
+  end
+end
