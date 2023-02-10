0965bdb3c2230beca0fc52ec9d583b612d1f5260
Merdan D <merdan.durdiyev@gmail.com>
2023-02-10 16:53:57 +0500

    Formats Recipe halal/kosher/vegetarian qualities as badges, using view helpers

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,19 @@
 module RecipesHelper
+
+  def halal_badge(recipe)
+    "<span class='badge bg-primary'>halal</span>".html_safe if recipe.is_halal
+  end
+
+  def kosher_badge(recipe)
+    "<span class='badge bg-warning'>kosher</span>".html_safe if recipe.is_kosher
+  end
+
+  def vegetarian_badge(recipe)
+    "<span class='badge bg-success'>vegetarian</span>".html_safe if recipe.is_vegetarian
+  end
+
+  def recipe_badges(recipe)
+    "#{halal_badge(recipe)} #{kosher_badge(recipe)} #{vegetarian_badge(recipe)}".html_safe
+  end
+
 end
