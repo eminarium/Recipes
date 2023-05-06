7c62f2a6e4bd0607f4033fac5a52c0f57d44411d
Merdan D <merdan.durdiyev@gmail.com>
2023-05-07 01:28:07 +0500

    Removes excess commented factories from factory bile

--- spec/factories.rb
+++ spec/factories.rb
@@ -26,27 +26,4 @@ FactoryBot.define do
     recipe { create(:recipe) }
   end
 
-  # factory :workout_exercise do
-  #   workout { create(:workout) }
-  #   exercise { create(:exercise, workout: workout) }
-  #   repetitions { 3 }
-  # end
-
-  # factory :workout do
-  #   name { Faker::Lorem.words.join(" ") }
-  #   category { create(:category) }
-  #   level { Workout.levels.keys.sample }
-  #   description { Faker::Lorem.sentence }
-  # end
-
-  # factory :exercise do
-  #   name { Faker::Lorem.words.join(" ") }
-  #   category { create(:category) }
-  #   description { Faker::Lorem.sentence }
-  # end
-
-  # factory(:category) do
-  #   name { Faker::Lorem.words.join(" ") }
-  #   description { Faker::Lorem.sentence }
-  # end
 end
